30d96fba81b204eb3c367f85b2e61a0f376a92df
Sayali Sanjay Misal <misalsayali24@gmail.com>
2026-09-07 19:05:51 +0530

    Refactor CRC sender code for clarity and structure

--- Cyclic Redundancy Check Sender Side.cpp
+++ Cyclic Redundancy Check Sender Side.cpp
@@ -22,11 +22,13 @@ int main()
     int p = n +(m - 1);
     int message[p]; 
      for (int i = 0; i < n; i++)
+    {
         message[i] = frame[i];
-
+    }
     for (int i = n; i < p; i++)
+    {
         message[i] = 0;
-
+    }
     for (int i = 0; i < n; i++)
     {
         if (message[i] == 1)
@@ -37,12 +39,17 @@ int main()
     }
     cout << "CRC: ";
     for (int i = n; i < p; i++)
+    {
         cout << message[i];
+    }
     cout << "\nTransmitted Frame: ";
     for (int i = 0; i < n; i++)
+    {
         cout << frame[i];
-
+    }
     for (int i = n; i < p; i++)
+    {
         cout << message[i];
+    }
     return 0;
 }
